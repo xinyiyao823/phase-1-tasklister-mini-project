74f89bf98d678e2b1b7d0710f0c56f41fafa264d
+ <+>
2021-09-06 16:46:18 -0500

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
+
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  //your code here
+  
+  document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    const input = document.getElementById('new-task-description').value; // 'Feed Moose'
+    const list = document.getElementById('tasks');
+    createToDo(input)
+  })
+
 });
+
+// 1.) Grab form with inputs for adding things to list
+
+
+function createToDo(text) {
+  let l = document.createElement('li') //creating list with bullets
+  l.textContent = text; // setting content of list to the input of form
+  let btn = document.createElement('button')
+  btn.textContent = 'x'
+  btn.addEventListener('click', handleDelete)
+  document.querySelector('ul').appendChild(l); //append list to unordered list already on page
+  l.appendChild(btn)
+}
+
+function handleDelete() {
+  const input = document.getElementById('new-task-description').value; // 'Feed Moose'
+  const list = document.getElementById('tasks');
+  document.querySelector('li').remove()
+}
